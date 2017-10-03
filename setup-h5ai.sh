fcb7845b17c775865b62a79f71a04fd3b128d354
Grey Christoforo <grey@christoforo.net>
2017-10-03 15:48:59 +0200

    Update setup-h5ai.sh

--- setup-h5ai.sh
+++ setup-h5ai.sh
@@ -13,3 +13,21 @@ pacman -S --noprogressbar --noconfirm --needed imagemagick
 # install h5ai
 su docker -c 'pacaur -S --noprogressbar --noedit --noconfirm h5ai'
 cp -a /usr/share/webapps/h5ai /srv/http/_h5ai
+mv /usr/share/webapps/h5ai/.htaccess /usr/share/webapps/h5ai/.bak.htaccess
+cp /usr/share/webapps/h5ai/.bak.htaccess /etc/httpd/conf/extra/h5ai.conf
+
+#indent the whole file
+sed  's/^/  /' -i /etc/httpd/conf/extra/h5ai.conf
+
+sed -i '1s;^;<Directory /usr/share/webapps/h5ai/>;' /etc/httpd/conf/extra/h5ai.conf
+sed -i '1s;^;Alias /h5ai "/usr/share/webapps/h5ai";' /etc/httpd/conf/extra/h5ai.conf
+sed -i '$a </Directory>' /etc/httpd/conf/extra/h5ai.conf
+
+sed -i '$a Include conf/extra/h5ai.conf' /etc/httpd/conf/httpd.conf
+
+# add and enable .htaccess
+#ADD .htaccess /srv/http/
+#RUN sed -i '/<Directory "\/srv\/http">/,/<\/Directory>/ s/AllowOverride None/AllowOverride All/g' /etc/httpd/conf/httpd.conf
+
+# treat .qc files as text files
+#RUN sed -i 's:\["\*\.text", "\*\.txt"\],:\["\*\.text", "\*\.txt", "\*\.qc"\],:g' /srv/http/_h5ai/conf/types.json
