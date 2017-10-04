f61a2476094df691f4f5f6f02a6a7a568730c113
Grey Christoforo <grey@christoforo.net>
2017-10-04 11:08:06 +0100

    Update setup-h5ai.sh

--- setup-h5ai.sh
+++ setup-h5ai.sh
@@ -21,8 +21,8 @@ sed  's/^/  /' -i /etc/httpd/conf/extra/h5ai.conf
 sed -i '1i\\' /etc/httpd/conf/extra/h5ai.conf
 sed -i '1s;^;<Directory /usr/share/webapps/h5ai/>;' /etc/httpd/conf/extra/h5ai.conf
 sed -i '1i\\' /etc/httpd/conf/extra/h5ai.conf
-sed -i '1s;^;Alias /h5ai "/usr/share/webapps/h5ai";' /etc/httpd/conf/extra/h5ai.conf
 sed -i '1i\\' /etc/httpd/conf/extra/h5ai.conf
+sed -i '1s;^;Alias /h5ai "/usr/share/webapps/h5ai";' /etc/httpd/conf/extra/h5ai.conf
 sed -i '$a </Directory>' /etc/httpd/conf/extra/h5ai.conf
 
 sed -i '$a Include conf/extra/h5ai.conf' /etc/httpd/conf/httpd.conf
