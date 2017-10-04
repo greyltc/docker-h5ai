84ac9eaf0f5385b7e4148575d95e657296ce9b83
Grey Christoforo <grey@christoforo.net>
2017-10-04 12:33:59 +0100

    Update setup-h5ai.sh

--- setup-h5ai.sh
+++ setup-h5ai.sh
@@ -2,34 +2,25 @@
 
 # h5ai deps
 pacman -S --noprogressbar --noconfirm --needed zip
-pacman -S --noprogressbar --noconfirm --needed ffmpeg
-pacman -S --noprogressbar --noconfirm --needed ghostscript
-pacman -S --noprogressbar --noconfirm --needed openexr
-pacman -S --noprogressbar --noconfirm --needed openjpeg2
-pacman -S --noprogressbar --noconfirm --needed libwmf
-pacman -S --noprogressbar --noconfirm --needed libwebp
-pacman -S --noprogressbar --noconfirm --needed imagemagick
+pacman -S --noprogressbar --noconfirm --needed ffmpeg-headless
+#pacman -S --noprogressbar --noconfirm --needed ghostscript
+#pacman -S --noprogressbar --noconfirm --needed openexr
+#pacman -S --noprogressbar --noconfirm --needed openjpeg2
+#pacman -S --noprogressbar --noconfirm --needed libwmf
+#pacman -S --noprogressbar --noconfirm --needed libwebp
+#pacman -S --noprogressbar --noconfirm --needed imagemagick
+pacman -S --noprogressbar --noconfirm --needed tar
+pacman -S --noprogressbar --noconfirm --needed coreutils
 
 # install h5ai
 su docker -c 'pacaur -S --noprogressbar --noedit --noconfirm h5ai'
-cp -a /usr/share/webapps/h5ai /srv/http/_h5ai
-mv /usr/share/webapps/h5ai/.htaccess /usr/share/webapps/h5ai/.bak.htaccess
-cp /usr/share/webapps/h5ai/.bak.htaccess /etc/httpd/conf/extra/h5ai.conf
+mv /usr/share/webapps/h5ai /srv/http/_h5ai
 
-#indent the whole file
-sed  's/^/  /' -i /etc/httpd/conf/extra/h5ai.conf
-sed -i '1i\\' /etc/httpd/conf/extra/h5ai.conf
-sed -i '1s;^;<Directory /usr/share/webapps/h5ai/>;' /etc/httpd/conf/extra/h5ai.conf
-sed -i '1i\\' /etc/httpd/conf/extra/h5ai.conf
-sed -i '1i\\' /etc/httpd/conf/extra/h5ai.conf
-sed -i '1s;^;Alias /h5ai "/usr/share/webapps/h5ai";' /etc/httpd/conf/extra/h5ai.conf
-sed -i '$a </Directory>' /etc/httpd/conf/extra/h5ai.conf
+# use h5ai for index
+sed -i 's,DirectoryIndex index.html,DirectoryIndex index.html /_h5ai/public/index.php,g' /etc/httpd/conf/httpd.conf
 
-sed -i '$a Include conf/extra/h5ai.conf' /etc/httpd/conf/httpd.conf
-
-# add and enable .htaccess
-#ADD .htaccess /srv/http/
-#RUN sed -i '/<Directory "\/srv\/http">/,/<\/Directory>/ s/AllowOverride None/AllowOverride All/g' /etc/httpd/conf/httpd.conf
+#enable .htaccess
+sed -i '/<Directory "\/srv\/http">/,/<\/Directory>/ s/AllowOverride None/AllowOverride All/g' /etc/httpd/conf/httpd.conf
 
 # treat .qc files as text files
 #RUN sed -i 's:\["\*\.text", "\*\.txt"\],:\["\*\.text", "\*\.txt", "\*\.qc"\],:g' /srv/http/_h5ai/conf/types.json
